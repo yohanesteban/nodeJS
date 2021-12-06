82d23214eaf1b2721deadb5a007947ec70cd04dc
yohanesteban <emunozmontero04@gmail.com>
2021-12-06 16:41:18 -0500

    hola

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const express = requiere('express')
+const express = require('express')
+const mongose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors)
+
+mongoose.connect(process.env.MONGO_URI,  { useNewURLParser: true, useUnifiedTopology: true})
+
+app.get('*', (req, res) => {
+res, send('chanchito feliz')
+})
+module, exports = app
